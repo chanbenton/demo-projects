0d4aa5b4b4b3b32d6fc5c53b7e6c4b90a23cd0c6
Benton Chan <chan.benton@gmail.com>
2018-03-19 16:09:09 -0400

    Init starbase proj, added Luke Skywalker JSON fetch function

---
+++ VueJS/starbase/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h3>{{title}}</h3>
+    <button class="btn btn-primary" @click="fetchCharacter">Test Character Fetch</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      title: 'Generate Your Team'
+    }
+  },
+  methods: {
+    fetchCharacter(){
+      fetch('https://swapi.co/api/people/1', {
+        mode: 'cors',
+        method: 'GET'
+      })
+        .then(response => response.json())
+        .then(json => console.log(json))
+    }
+
+  }
+}
+</script>
