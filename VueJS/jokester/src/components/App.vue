e5c570be78ab8787b898fb525a97b021aa040537
Benton Chan <chan.benton@gmail.com>
2018-04-07 23:55:48 -0400

    MVP of app - delivers 10 jokes by use of API (Vuex)

---
+++ VueJS/jokester/src/components/App.vue
@@ -0,0 +1,31 @@
+<template>
+	<div id="app">
+		Got jokes?
+		<button class="btn btn-primary" @click="initJokes">
+		Add 10 random jokes</button><br><br>
+		
+		<div class="container">
+			<div class="row form-group">
+				<Joke 
+					v-for="(joke, index) in $store.state.jokes"
+					:joke="joke"
+					key="index" 
+				/>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	import {mapActions} from 'vuex'
+	import Joke from './Joke.vue'
+
+	export default{
+		methods: mapActions([
+			'initJokes'
+		]),
+		components:{
+			Joke
+		}
+	}
+</script>
